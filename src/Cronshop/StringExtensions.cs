9026834c42ac95c88193632e66b3219ada37ec48
jp.berdel <jp.berdel@googlemail.com>
2011-07-09 17:59:50 +0200

    added niob server

---
+++ src/Cronshop/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Niob.SimpleHtml;
+
+namespace Cronshop
+{
+    public static class StringExtensions
+    {
+        public static string Encode(this string str)
+        {
+            return Html.Encode(str);
+        }
+    }
+}
