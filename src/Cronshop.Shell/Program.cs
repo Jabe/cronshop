3ea04a7ce58e18c2a0d05e620ca958ff1149f643
jp.berdel <jp.berdel@googlemail.com>
2011-07-09 17:05:30 +0200

    hello world

---
+++ src/Cronshop.Shell/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cronshop.Shell
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/Cronshop/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cronshop
+{
+    public class Class1
+    {
+    }
+}
